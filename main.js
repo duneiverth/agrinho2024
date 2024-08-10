2e5eab2194b593ffdf62c9afe1c5a97b3e4d4260
Duda <105284664+duneiverth@users.noreply.github.com>
2024-08-09 21:44:50 -0300

    ok

---
+++ main.js
@@ -0,0 +1,13 @@
+const botao = document.querySelector('.botao-1');
+
+botao.forEach(botao => botao.addEventListener('click'), () => botaoClicked(botao));
+
+const botaoClicked = (botao) => {
+    const content = document.querySelector('content');
+
+    contents.forEach (content => content.classlist.remove('show'));
+
+    const contentid = tab.getAttribute('content-id');
+
+    console.log(contentId);
+}
